ca5f81714b65906d6943f7aaf54d27b148c46135
denofdhamodhar <dhamodhar459@gmail.com>
2026-06-06 17:19:27 +0530

    layout issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="flex justify-center items-center font-Montserrat">
-      <div className="bg-slate-100 shadow-xl border shadow-slate-300 max-w-74 330b:max-w-xs 360b:max-w-88  w-full my-4 h-dvh overflow-y-hidden rounded-3xl text-white ">
+      <div className="bg-slate-100 shadow-xl border shadow-slate-300 max-w-74 330b:max-w-xs 360b:max-w-84  w-full my-4 min-h-screen overflow-y-hidden rounded-3xl text-white ">
         <Outlet />
       </div>
     </div>
